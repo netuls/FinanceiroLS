6ab474479bff8d9a0aac4c4fb7e2856ea2e278f4
netuls <56983125+netuls@users.noreply.github.com>
2026-03-19 21:17:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
+      return cache.addAll(["/"]);
     })
   );
 });
